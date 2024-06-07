454952a7657225ccf1a3a1b3f9dc948ba7eb43bf
Evanchalin <119653875+Evanchalin-C-K@users.noreply.github.com>
2024-06-07 08:40:03 +0530

    Add files via upload

--- load_testing.py
+++ load_testing.py
@@ -1,7 +1,9 @@
+# Importing necessary modules and classes from Locust
 from locust import SequentialTaskSet, HttpUser, task, events, TaskSet, constant_pacing, between, tag
 from locust.event import EventHook
 import logging
 
+# EventHook for sending notification
 send_notification = EventHook()
 
 
@@ -12,11 +14,55 @@ def notification(req_id, message="success", **kwargs):
 send_notification.add_listener(notification)
 
 
+# Define a class for inheriting sequential task
+class LoadTest(SequentialTaskSet):
+    wait = between(1, 2)
+    url = "https://65c0f239dc74300bce8d0afe.mockapi.io/users"
+    data = {"Name": "Priya", "City": "Chennai", "Country": "India"}
+
+    # Define task to be performed by the simulated user
+    @task
+    # Perform HTTP GET request
+    def get_data(self):
+        with self.client.get(self.url + '/' + "8", catch_response=True) as response:
+            if response.status_code == 200:
+                print(response.json())
+        with self.client.get(self.url + '/' + "12", catch_response=False) as response:
+            print(response)
+        self.interrupt()
+
+    @task
+    # Perform HTTP POST request
+    def post_data(self):
+        with self.client.post(url=self.url, data=self.data, catch_response=True) as response:
+            if response.status_code == 201:
+                response.success()
+                logging.info("data posted successfully\n")
+            else:
+                response.failure()
+                logging.info("error\n")
+            self.interrupt()
+
+    @task
+    # Perform HTTP UPDATE request
+    def update_data(self):
+        response = self.client.put(self.url + "/51", data={"City": "Chennai"})
+        if response.status_code == 200:
+            print("Status : Success")
+            logging.info("Success")
+        else:
+            print("Status : Failure")
+            logging.info("Failed")
+
+
+# Define a class for inheriting TaskSet
 class Mytask(TaskSet):
-    wait = constant_pacing(1)
+    # Set a constant pacing of 2 second between tasks
+    wait = constant_pacing(2)
     url = "https://65c0f239dc74300bce8d0afe.mockapi.io/users"
 
     @task
+    # Perform HTTP DELETE request
     def delete_data(self):
         length = len(self.client.get(self.url).json())
         for i in range(51, length):
@@ -28,37 +74,13 @@ class Mytask(TaskSet):
                 self.interrupt()
 
 
-class LoadTest(SequentialTaskSet):
-    wait = between(1, 2)
-    url = "https://65c0f239dc74300bce8d0afe.mockapi.io/users"
-    data = {"Name": "Priya", "City": "Chennai", "Country": "India"}
-
-    @task
-    def get_data(self):
-        with self.client.get(self.url + '/' + "8", catch_response=True) as response:
-            if response.status_code == 200:
-                print(response.json())
-        with self.client.get(self.url + '/' + "12", catch_response=False) as response:
-            print(response)
-        self.interrupt()
-
-    @task
-    def post_data(self):
-        with self.client.post(url=self.url, data=self.data, catch_response=True) as response:
-            if response.status_code == 201:
-                response.success()
-                logging.info("data posted successfully")
-            else:
-                response.failure()
-                logging.info("error")
-            self.interrupt()
-
-
+# Event handler for each request
 @events.request.add_listener
 def on_request(name, request_type, response_time, **kwargs):
     print(name, request_type, response_time, kwargs)
 
 
+# Define a class inheriting HttpUser, representing simulated user
 class LoadTest_(HttpUser):
     host = "https://65c0f239dc74300bce8d0afe.mockapi.io/users"
     tasks = [Mytask, LoadTest]
